7df59becdcf3692e7dde562c5676620039d29f46
Brandi Starks <brandis@dorothysparadise.com>
2025-07-02 17:47:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        Hello from The Soft Syndicate ✨
+      </h1>
+    </div>
+  )
+}
