0e11ab2f15b803df733636ab5479f67bb623791d
Brandi Starks <brandis@dorothysparadise.com>
2025-07-02 13:02:27 -0400

    Update next.config.js

--- The-Soft-Syndicate-App-main/next.config.js
+++ The-Soft-Syndicate-App-main/next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ["yourdomain.com"], // replace with real domains if using <Image />
-  },
 };
 
 module.exports = nextConfig;
